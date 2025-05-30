1e864ce9ae684e4e396d4e5707959f472aaaac30
Lucas <natanaelzigaran@gmail.com>
2025-05-29 21:51:32 -0300

    Se Añadio un listener

--- trabajo-practico-3-lucas-zigaran/assets/js/script.js
+++ trabajo-practico-3-lucas-zigaran/assets/js/script.js
@@ -1,10 +1,12 @@
-document.getElementById('traerApi').addEventListener('click', async () => {
-  try {
-    const respuesta = await fetch('https://api.ejemplo.com/datos');
-    const data = await respuesta.json();
-    document.getElementById('resultado').textContent = JSON.stringify(data, null, 2);
-  } catch (error) {
-    console.error('Error al obtener datos:', error);
-    document.getElementById('resultado').textContent = 'Error al cargar los datos';
-  }
+const boton = document.getElementById('traerApi')
+
+boton.addEventListener('click', (datos) => {
+    fetch ("https://dragonball-api.com/api/characters?name=goku").then
+    ( Response => { 
+        return Response.json()
+    }).then( datos => {
+        console.log(datos)
+    })
+    
 });
+
